d03a74ae241a17cc7490949f5a7b0d9fce4cf5bb
Ankit08012003 <ankitkumar221818@gmail.com>
2026-04-06 19:03:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function updateClock() {
+  const now = new Date();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  // leading zero
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  const timeString = `${hours}:${minutes}:${seconds}`;
+
+  document.getElementById("time").textContent = timeString;
+}
+
+// run every second
+setInterval(updateClock, 1000);
+
+// run immediately
+updateClock();
